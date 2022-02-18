c08f87d83220aa9578429b4815d3910444881a49
Daniel <djsplus1@gmail.com>
2022-02-17 21:07:44 -0600

    updated sketch

--- RGB_control_sketch/RGB_control_sketch.ino
+++ RGB_control_sketch/RGB_control_sketch.ino
@@ -5,25 +5,88 @@
 //B    ->  GPIO18
 
 
-
+//class created to handle the RGB LED
 class RGBLed {
   private:
-    int const redLedPin = 23;
-    int const greenLedPin = 19;
-    int const blueLedPin = 18;
+    const int redLedPin = 23;
+    const int greenLedPin = 19;
+    const int blueLedPin = 18;
 
     bool redLedState;
     bool greenLedState;
     bool blueLedState;
   public:
+    //constructor method without parameters
     RGBLed() {
       redLedState = false;
       greenLedState = false;
       blueLedState = false;
+
+      pinMode(redLedPin, OUTPUT);
+      pinMode(greenLedPin, OUTPUT);
+      pinMode(blueLedPin, OUTPUT);
+
+      digitalWrite(redLedPin, LOW);
+      digitalWrite(greenLedPin, LOW);
+      digitalWrite(blueLedPin, LOW);
+    }
+    //constructor method with parameters
+    RGBLed(bool r, bool g, bool b) {
+      pinMode(redLedPin, OUTPUT);
+      pinMode(greenLedPin, OUTPUT);
+      pinMode(blueLedPin, OUTPUT);
+
+      setRedLedState(r);
+      setGreenLedState(g);
+      setBlueLedState(b);
+    }
+    //setters
+    void setRedLedState(bool s) {
+      if (s) {
+        redLedState = s;
+        digitalWrite(redLedPin, HIGH);
+      } else {
+        redLedState = s;
+        digitalWrite(redLedPin, LOW);
+      }
+    }
+
+    void setGreenLedState(bool s) {
+      if (s) {
+        greenLedState = s;
+        digitalWrite(greenLedPin, HIGH);
+      } else {
+        greenLedState = s;
+        digitalWrite(greenLedPin, LOW);
+      }
+    }
+
+    void setBlueLedState(bool s) {
+      if (s) {
+        blueLedState = s;
+        digitalWrite(blueLedPin, HIGH);
+      } else {
+        blueLedState = s;
+        digitalWrite(blueLedPin, LOW);
+      }
+    }
+    //getters
+    bool getRedLedState() {
+      return redLedState;
+    }
+
+    bool getGreenLedState() {
+      return greenLedState;
+    }
+
+    bool getBlueLedState() {
+      return blueLedState;
     }
 
 };
 
+//instantiates the RGBLed object
+RGBLed rgb;
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,5 +95,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  rgb.setRedLedState(true);
+  delay(1000);
+  rgb.setRedLedState(false);
+  rgb.setGreenLedState(true);
+  delay(1000);
+  rgb.setGreenLedState(false);
+  rgb.setBlueLedState(true);
+  delay(1000);
+  rgb.setBlueLedState(false);
 }
